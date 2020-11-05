c5233d9c893da4c1f8890f5b2195cbae911b9d4d
jornelas4 <jornelas4@my.wctc.edu>
2020-11-04 21:28:15 -0600

    Fixed

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 This project contains 12 lines of code that need to be fixed! Some are in the HTML, some are in the JavaScript.
 
 Happy bug hunting!
+
+HTML
+Linee 22 had the wrong attribute set.  It was name wasn was supposed to be Id
+Line 34
+
+
+javascript
+
+line 2 extra parentheses
+Line 4 missing event inside parantheses
+Line 13 matn is lower case
+line 14 max is Uppercase
--- quizCalc.js
+++ quizCalc.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
-    $("form").submit(calculate());
+    $("form").submit (calculate);
 
-    function calculate() {
+    function calculate(event) {
         event.preventDefault();
 
-        var quiz1 = $("#quiz1").val();
-        var quiz2 = $("#quiz2").val();
-        var quiz3 = $("#quiz3").val();
+        var quiz1 = parseFloat ($("#quiz1").val());
+        var quiz2 = parseFloat($("#quiz2").val());
+        var quiz3 = parseFloat($("#quiz3").val());
 
         var average = (quiz1 + quiz2 + quiz3) / 3;
 
-        var min = math.min(quiz1, quiz2, quiz3);
-        var max = Math.Max(quiz1, quiz3, quiz3);
+        var min = Math.min(quiz1, quiz2, quiz3);
+        var max = Math.max(quiz1, quiz3, quiz3);
 
         var gradeToPass = (73 * 4) - (quiz1 + quiz2 + quiz3);
 
         $("#average").text(average.toFixed(1) + "%");
-        $("minMax").text(`${min}/${max}`);
-        $("#quiz4Grade").text(gradeToPass.tofixed(1));
+        $("#minMax").text(`${min}/${max}`);
+        $("#quiz4Grade").text(gradeToPass.toFixed(1));
     }
 });
